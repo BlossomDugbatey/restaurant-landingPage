88a2df39d46ee5ba744285c3129f20c7ea3f7abf
Dugbatey Blossom <blossomddb@gmail.com>
2021-12-06 09:18:35 -0800

    View menu function added to view menu button(commit on 'master')

--- landing.js
+++ landing.js
@@ -5,3 +5,22 @@ $(window).scroll(function () {
         $("nav").removeClass("sticky")
     }
 });
+
+// $(window).load(function() {
+//     $(".btn-nav").on("click tap", function() {
+//       $(".nav-items").toggleClass("showNav hideNav").removeClass("hidden");
+//       $(this).toggleClass("animated");
+//     });
+//   });
+
+document.getElementById("menu").addEventListener('click', menuDisplay)
+function menuDisplay(){
+    var menuList = document.getElementById("menu-image")
+    if (menuList.style.display === "none"){
+        menuList.style.display = "block"
+    }else {
+        menuList.style.display = "none"
+    }
+    console.log(menuDisplay)
+};
+
